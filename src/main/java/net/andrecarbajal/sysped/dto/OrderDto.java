cd8c80e2b8026b2bad7e21af02c8b98b17f7c757
andre-carbajal <carbajalvargasandre@gmail.com>
2025-11-14 21:29:20 -0500

    refactor: convert OrderDto class to a record for improved readability and conciseness

--- src/main/java/net/andrecarbajal/sysped/dto/OrderDto.java
+++ src/main/java/net/andrecarbajal/sysped/dto/OrderDto.java
@@ -6,21 +6,12 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public class OrderDto {
-    public Long id;
-    public Integer tableNumber;
-    public LocalDateTime dateAndTimeOrder;
-    public OrderStatus status;
-    public BigDecimal totalPrice;
-    public List<OrderItemResponseDto> details;
-
-    public OrderDto(Long id, Integer tableNumber, LocalDateTime dateAndTimeOrder, OrderStatus status, BigDecimal totalPrice, List<OrderItemResponseDto> details) {
-        this.id = id;
-        this.tableNumber = tableNumber;
-        this.dateAndTimeOrder = dateAndTimeOrder;
-        this.status = status;
-        this.totalPrice = totalPrice;
-        this.details = details;
-    }
+public record OrderDto(
+        Long id,
+        Integer tableNumber,
+        LocalDateTime dateAndTimeOrder,
+        OrderStatus status,
+        BigDecimal totalPrice,
+        List<OrderItemResponseDto> details
+) {
 }
-
